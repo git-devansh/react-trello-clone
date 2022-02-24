8f3ac2afd84a1370028b9599d3e5bfb4e99bef56
Devansh <72165346+git-devansh@users.noreply.github.com>
2022-02-24 12:01:24 +0100

    code added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import Store from "./store";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import "./index.css";
+import { createGlobalStyle } from "styled-components";
+import $ from "jquery";
+import { PersistGate } from "redux-persist/integration/react";
+
+const { persistor, store } = Store();
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    background-color: #186353;
+    box-sizing: border-box;
+    transition: all 0.5s ease-in;
+  }
+`;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <GlobalStyle />
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+$(document).bind("DOMNodeRemoved", function (e) {
+  console.log("Removed: " + e.target.nodeName);
+});
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
